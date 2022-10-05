ab53a73e7cd36b92b142ffc082ef8f4d4382ed72
rongray916 <115110823+rongray916@users.noreply.github.com>
2022-10-05 20:36:41 +0100

    Add files via upload

---
+++ js-quizzes/questions1.js
@@ -0,0 +1,16 @@
+// creating an array and passing the number, questions, answer, and options
+let questions = [
+    {
+    numb: 1,
+    question: "How many teeth does an adult dog have ?",
+    answer: "42",
+    options: [
+      "26",
+      "18",
+      "42",
+      "39"
+    ]
+
+},
+ 
+];
---
+++ js-quizzes/questions22.js
@@ -0,0 +1,16 @@
+// creating an array and passing the number, questions, answer, and options
+let questions = [
+    {
+        numb: 22,
+        question: "Your Question is Here",
+        answer: "option 2",
+        options: [
+        "option 1",
+        "option 2",
+        "option 3",
+        "option 4"
+      ]
+    
+     },
+    
+     ];
